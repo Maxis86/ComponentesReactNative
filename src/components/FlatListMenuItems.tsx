7d98845f47020d4269f36c5b1004a0644bf1ba38
Maxis86 <maximilianoangelchamarro@gmail.com>
2021-07-02 09:45:58 -0300

    Se agrega el icon y la flecha al FlatList

--- src/components/FlatListMenuItems.tsx
+++ src/components/FlatListMenuItems.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { View } from 'react-native'
+import  Icon  from 'react-native-vector-icons/Ionicons'
 import { MenuItem } from '../interfaces/appInterfaces'
 
 interface Props{
@@ -9,14 +11,38 @@ interface Props{
 
 export const FlatListMenuItems = ({menuItem}:Props) => {
     return(
-        <View style>
-            <Text>{menuItem.name} - {menuItem.icon}</Text>
-        </View>
+
+        <TouchableOpacity
+            activeOpacity={0.5} // para que no se haga tan transparente
+        >
+            <View style = {styles.container}>
+                <Icon
+                    name={menuItem.icon}
+                    color="grey"
+                    size= {19}
+                />
+                <Text style = {styles.itemText}>
+                    {menuItem.name} - {menuItem.icon}
+                </Text>
+
+                <View style={{flex:1}}/>
+                    <Icon
+                        name= "chevron-forward-outline"
+                        color="grey"
+                        size= {19}
+                    />
+            </View>
+        </TouchableOpacity>
+        
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flexDirection: 'row'
+    },
+    itemText: {
+        marginLeft: 10,
+        fontSize: 15
     }
 });
